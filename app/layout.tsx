2119c32261f228a505d38b91d0dc3ab77b93183e
Anand Shukla <anandshukla.web@gmail.com>
2024-04-04 00:14:59 +0530

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,17 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 
 import "../styles/globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter"
 });
 
-const spaceGrotesk = Space_Grotesk({ 
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-spaceGrotesk"
@@ -21,7 +22,7 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: "StackOverflow",
   description: "A community-driven platform for asking and answering questions. Get help, share knowledge and collaborate with developers from around the world.",
-  icons:{
+  icons: {
     icon: '/assets/images/site-logo.svg'
   }
 };
@@ -32,19 +33,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-primary-500"
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500"
+            }
+          }}
+        >
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+
   );
 }
